c0aea955e2d684cc624758a270c516186dfa7cfb
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-10-11 03:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let res = "";
+	while(l--){
+	    res+= char.charAt(Math.floor(Math.random()*char.length));
+	}
+	return res;
 }
 
 // Do not change the code below.
